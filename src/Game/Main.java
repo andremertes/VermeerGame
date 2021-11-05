6fa5f4401db613f1a832fec4aa0c4b1eeb1ee757
Andre Mertes <andremertes@gmail.com>
2021-11-06 00:29:10 +0100

    Set Main Scene

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,5 +1,6 @@
 package Game;
 
+import Gui.ViewManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,10 +11,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.show();
+
+        try {
+
+            ViewManager manager = new ViewManager();
+            primaryStage = manager.getMainStage();
+            primaryStage.setTitle("Vermeer - Die Kunst zu erben! by Raphael Kropp und Andre Mertes");
+            primaryStage.show();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 
 

979aa5cddaf5378cd8323da9cb83c2ad67882739
Andre Mertes <andremertes@gmail.com>
2021-11-17 13:54:32 +0100

    updated semantic

--- src/Plantage/Grass.java
+++ src/Plantage/Grass.java
@@ -2,6 +2,8 @@ package Plantage;
 
 public class Grass extends MyField{
 
+    private final static String BACKGROUND_IMAGE = "/Gui/Resources/ .png";  // grass.png
+
     public Grass() {
 
         this.buildable = true;
--- src/Plantage/Rock.java
+++ src/Plantage/Rock.java
@@ -2,6 +2,8 @@ package Plantage;
 
 public class Rock extends MyField {
 
+    private final static String BACKGROUND_IMAGE = "/Gui/Resources/ .png";  // rock.png
+
     public Rock() {
 
         this.buildable = false;

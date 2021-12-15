eddd4f7de92937c5388e8e8c4222d437251f2313
Andre Mertes <andremertes@gmail.com>
2021-12-15 13:02:53 +0100

    JavaFX-Runtime-Komponenten fehlen

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,13 +1,25 @@
 package Game;
 
 import Gui.ViewManager;
+import javafx.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.ImageCursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.show();
 
         try {
 
@@ -23,5 +35,9 @@ public class Main extends Application {
         }
 
     }
+
+    //  public static void main(String[] args) {
+    // launch(args);
+    //}
 
 }

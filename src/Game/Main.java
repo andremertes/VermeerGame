4dee5e0df48c7b6367e02e33bc575980a54e3747
Andre Mertes <andremertes@gmail.com>
2021-12-15 11:02:50 +0100

    optimizing JavaFX imports -> still causes crashs on M1

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -2,22 +2,12 @@ package Game;
 
 import Gui.ViewManager;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.ImageCursor;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //primaryStage.setTitle("Hello World");
-        //primaryStage.setScene(new Scene(root, 300, 275));
-        //primaryStage.show();
 
         try {
 
@@ -34,8 +24,4 @@ public class Main extends Application {
 
     }
 
-
-  //  public static void main(String[] args) {
-       // launch(args);
-    //}
 }
